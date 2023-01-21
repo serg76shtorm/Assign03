a4a5dab17fc309f27801a3e615e238942be02927
serg76shtorm <serg76shtorm@gmail.com>
2023-01-21 07:57:00 +0200

    assign03

---
+++ app/src/main/java/com/sserhiichyk/assign03/ContactsProfile.kt
@@ -0,0 +1,11 @@
+package com.sserhiichyk.assign03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts_profile)
+    }
+}
